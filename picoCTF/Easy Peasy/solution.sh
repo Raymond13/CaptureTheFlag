a906537778db0daaf363358e52a49b1566ac1802
raymond13 <raymondleung13@ymail.com>
2023-05-27 01:05:50 +0800

    update

---
+++ picoCTF/Easy Peasy/solution.sh
@@ -0,0 +1,1 @@
+echo -e "00000000000000000000000000000\n" | nc mercury.picoctf.net 11188
--- picoCTF/crackme-py/solution.sh
+++ picoCTF/crackme-py/solution.sh
@@ -1,4 +1,4 @@
-#wget https://mercury.picoctf.net/static/8fc4e878bd6708031d67cb846f03c140/crackme.py
+wget https://mercury.picoctf.net/static/8fc4e878bd6708031d67cb846f03c140/crackme.py
 
 echo -e "1\n1" | python -c """
 from crackme import *
