e9186b905c5eed46b4e4eb1cf0c0e7026fd86446
raymond13 <raymondleung13@ymail.com>
2023-05-28 13:56:32 +0800

    Easy Peasy

--- picoCTF/Easy Peasy/solution.sh
+++ picoCTF/Easy Peasy/solution.sh
@@ -1,1 +1,24 @@
-echo -e "00000000000000000000000000000\n" | nc mercury.picoctf.net 11188
+python3 -c "print('\x00'*(50000-32)+'\n'+'\x00'*32+'\n')" | nc mercury.picoctf.net 11188 > temp
+
+python3 -c """
+with open('temp', 'r') as fh:
+    data = fh.read().split('\n')
+
+encrypted_flag, key = data[2], data[-3]
+
+flag = 'picoCTF{'
+for k, ef in zip([key[i:i+2] for i in range(0, len(key), 2)], [encrypted_flag[i:i+2] for i in range(0, len(encrypted_flag), 2)]):
+    #print(k, ef)
+    for i in range(1000):
+        temp = '{:02x}'.format(int(k, 16) ^ i)
+        if temp == ef:
+            #print(chr(i), end='')
+            flag += chr(i)
+
+else:
+    flag += '}'
+
+print(flag)
+"""
+
+rm temp
